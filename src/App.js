43bd73c03775f5ec6e94724e27eca50be1158b8e
Breno <francbreno@gmail.com>
2018-06-22 16:41:28 -0300

    add app routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Toolbar from './components/Toolbar';
-import Container from './components/Container';
-import Home from './Home';
+import Toolbar from 'app/components/Toolbar';
+import Container from 'app/components/Container';
+import Home from 'app/Home';
+import AllMatches from 'app/AllMatches';
+import Leaderboard from 'app/Leaderboard';
 
 import './App.css';
 import Logo from './pwa-logo.png';
 
 const App = () => (
-  <div>
-    <Toolbar />
-    <Home />
-  </div>
+  <Router>
+    <div>
+      <Toolbar />
+      <Route path="/" component={Home} exact />
+      <Route path="/matches" component={AllMatches} />
+      <Route path="/leaderboard" component={Leaderboard} />
+    </div>
+  </Router>
 );
 
 export default App;
