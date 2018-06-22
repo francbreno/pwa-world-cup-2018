3d6aeaafddf2037555d20387bb1acdae3e99c23f
Breno <francbreno@gmail.com>
2018-06-22 09:17:00 -0300

    correct sw catch log

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,6 +3,6 @@ export default function registerServiceWorker() {
     navigator.serviceWorker.register("./service-worker.js")
       .then(registration => {
         console.log(`SW registered with scope: ${registration.scope}`);
-      }).catch(err => console.err(`SW registration failed: ${registration}`));
+      }).catch(err => console.error(`SW registration failed: ${err}`));
   }
 };
