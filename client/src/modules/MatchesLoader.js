b71105d9f4b486646b82dcd3e9368a9e73a6a220
Breno <francbreno@gmail.com>
2018-07-02 16:55:41 -0300

    add a component to load data

---
+++ client/src/modules/MatchesLoader.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import ArrayUtil from 'helpers/array';
+
+import Message from 'components/Message';
+
+const FILTERS = [
+  'today',
+  'current',
+];
+
+const getFilter = name => FILTERS.includes(name) ? name : '';
+
+export default class MatchesLoader extends React.Component {
+  state = {
+    matches: [],
+    waiting: true,
+  };
+
+  componentDidMount() {
+    const { filter } = this.props;
+    console.log(`https://worldcup.sfg.io/matches/${getFilter(filter)}`);
+    fetch(`https://worldcup.sfg.io/matches/${getFilter(filter)}`)
+      .then(response => response.json())
+      .then(matches => matches.filter(match => {
+        return !(
+          match.home_team.code === 'TBD'
+          || match.away_team.code === 'TBD'
+        );
+      }))
+      .then(matches =>
+        this.setState(() => ({
+          matches,
+          waiting: false,
+        }))
+      );
+  }
+
+  render() {
+    const { matches, waiting } = this.state;
+    const { compact } = this.props;
+    const hasMatches =  Array.isArray(matches) && matches.length > 0;
+    return (
+      <div className="matches">
+        {waiting && <Message type="info">Loading...</Message>}
+        {!waiting && this.props.children(this.state)}
+      </div>
+    );
+  }
+}
